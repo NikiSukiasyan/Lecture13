9df82dc2f5093b856fca2500baed8fcab67045b6
Niki <119692266+NikiSukiasyan@users.noreply.github.com>
2024-10-16 21:56:50 +0400

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// console.log("Hello JavaScript!");
+// console.log(5);
+// console.error(2);
+// console.warn("ReEducate");
+// console.info("Hello World!!!!");
+// console.clear();
+// let vaxosleptopebi = "1";
+// let nikisleptopebi = "2";
+// var ცვლადის ტიპი ძველი
+// let ცვლადის ტიპი ახალი
+// const ცვლადის ტიპი უცვლადი
+// let vaxoMagaria = false;
+// console.log(vaxoMagaria);
+// string იგივე ტექსტი იწერება პრჭყალებში "აქ"
+// number რომელიც არის რიხცვი ან ციფრი
+// boolean რომელიც არის true ან false ჭეშმარიტი/მცდარი
+// bigInt იგივე ნამბერი უბრალოდ მთელი რიცხვი
+// null
+// undefined
+// let VaxosMankanebi = [];
+// console.log();
